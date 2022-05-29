c3f76e37969365da2badc822cff202e263581c68
Mariuskvaal <mariuskvaal1@gmail.com>
2022-05-29 17:17:03 +0200

    update js

---
+++ js/script.js
@@ -0,0 +1,129 @@
+
+// Question 1
+// 
+var integerValue = 8;
+var decimalValue = 5.5;
+
+console.log(integerValue)
+
+console.log(decimalValue)
+
+
+// Question 2
+//
+var firstworkingDayInAWeek = "Monday"; 
+var secondtworkingDayInAWeek = "Thuesday";
+var thirdworkingDayInAWeek = "Wednesday";
+var fourthtworkingDayInAWeek = "Thuesday";
+var fifhttworkingDayInAWeek = "Friday";
+
+// Connecting all the variables above into one
+
+var allWorkingDayInAWeek=                                                     firstworkingDayInAWeek + " " +                   secondtworkingDayInAWeek + " "+                   thirdworkingDayInAWeek + " " +                   fourthtworkingDayInAWeek + " " +                   fifhttworkingDayInAWeek;
+
+// Creating a console.log to check if my work functions correctly inside the web browser
+
+console.log(allWorkingDayInAWeek)
+
+
+// Question 3
+//
+var addition = 10 + 5; 
+var subtraction = 10 - 5;
+var multiplication = 10 * 5;
+var division = 10 / 5;
+
+
+console.log(addition)
+console.log(subtraction)
+console.log(multiplication)
+console.log(division)
+
+
+// Question 4
+//
+
+var passwordCorrect = false;
+
+console.log(passwordCorrect)
+
+
+// Question 5
+//
+
+var logTheNumber = "The frogs are angry"
+var lengthOfStrings = logTheNumber.length;
+
+console.log(lengthOfStrings)
+
+// Question 6
+//
+
+var timeElapsed;
+var totalPrice;
+
+// Question 7
+//
+
+var number1 = "53";
+var convertStringToNumber1 = parseInt(number1)
+
+console.log(convertStringToNumber1)
+
+var number2 = "44.6";
+var convertStringToNumber2 = parseFloat(number2)
+
+console.log(convertStringToNumber2)
+
+// Question 8
+//
+
+var pet = "donkey";
+var petDataType = typeof pet;
+console.log(petDataType)
+
+var age = 89;
+var ageDataType = typeof age;
+console.log(ageDataType)
+
+var discountApplied = true;
+var discountDataType = typeof discountApplied;
+console.log(discountDataType)
+
+var timeLeft;
+var timeLeftDataType = typeof timeLeft;
+console.log(timeLeftDataType)
+
+// Question 9
+//
+
+var change = "updating heading";
+var h1Container = document.querySelector("h1");
+
+h1Container.innerHTML = change
+
+console.log("h1Container:")
+
+// Question 10
+//
+var background = "lightskyblue"
+var backgroundContainer = document.querySelector("body");
+
+backgroundContainer.style.backgroundColor = background;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
